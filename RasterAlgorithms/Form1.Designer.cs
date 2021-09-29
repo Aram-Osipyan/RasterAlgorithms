596d0e20cfe4c73122dc6abf22e096cb60bc5057
Aramcho <osipyan.aram2017@ya.ru>
2021-09-30 01:50:13 +0300

    сделал первое задание

---
+++ RasterAlgorithms/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace RasterAlgorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(782, 61);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.Location = new System.Drawing.Point(666, 6);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(52, 52);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.FillPictureButton);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.Location = new System.Drawing.Point(724, 6);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(52, 52);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(293, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(52, 52);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.ColorChooseButton);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(203)))), ((int)(((byte)(110)))));
+            this.button6.Location = new System.Drawing.Point(235, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(52, 52);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.ColorChooseButton);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(112)))), ((int)(((byte)(85)))));
+            this.button5.Location = new System.Drawing.Point(177, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(52, 52);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.ColorChooseButton);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(67)))), ((int)(((byte)(147)))));
+            this.button4.Location = new System.Drawing.Point(119, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 52);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.ColorChooseButton);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(184)))), ((int)(((byte)(148)))));
+            this.button3.Location = new System.Drawing.Point(61, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 52);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.ColorChooseButton);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(118)))), ((int)(((byte)(117)))));
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 52);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.ColorChooseButton);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(118)))), ((int)(((byte)(117)))));
+            this.button1.Location = new System.Drawing.Point(-334, -21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 52);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(782, 365);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Transparent;
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.Location = new System.Drawing.Point(608, 6);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(52, 52);
+            this.button10.TabIndex = 9;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.BorderFillButton);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Transparent;
+            this.button11.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button11.BackgroundImage")));
+            this.button11.Location = new System.Drawing.Point(550, 6);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(52, 52);
+            this.button11.TabIndex = 9;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.DrawButton);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ RasterAlgorithms/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RasterAlgorithms
+{
+    enum FillingState
+    {
+        Default,
+        Draw,
+        SimpleFill,
+        PictureFill,
+        BorderFill
+    }
+    public partial class Form1 : Form
+    {
+        private Color _currentColor;
+        private Pen _currentPen;
+        private Pen _fillPen;
+        private Bitmap _mainBmp;
+        private Graphics _mainGraphics;
+        private Point lastPoint = Point.Empty;
+        private FillingState _fillingState;
+        private OpenFileDialog _openFileDialog;
+        private Bitmap _fillPicture;
+        public Form1()
+        {
+            InitializeComponent();
+            _currentPen = new Pen(Color.Black, 3);
+            _fillPen = new Pen(Color.Black, 1);
+            _mainBmp = new Bitmap(Width, Height);
+            _openFileDialog = new OpenFileDialog();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            _mainGraphics = Graphics.FromImage(_mainBmp);
+            pictureBox1.Image = _mainBmp;
+        }
+
+        private void ColorChooseButton(object sender, EventArgs e)
+        {
+            _currentColor = (sender as Button).BackColor;
+            _currentPen.Color = _currentColor;
+            _fillPen.Color = _currentColor;
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (_fillingState)
+            {
+                case FillingState.SimpleFill:
+                    AFill(e.Location);
+                    return;
+                case FillingState.PictureFill:
+                    BFill(e.Location);
+                    return;
+                case FillingState.BorderFill:
+                    CFill(e.Location);
+                    return;
+                case FillingState.Draw:
+                    lastPoint = e.Location;
+                    return;
+                case FillingState.Default:
+                    _fillingState = FillingState.Draw;
+                    break;
+            }
+        }
+
+        private void CFill(Point location)
+        {
+            Color freeAreaColor = _mainBmp.GetPixel(location.X, location.Y);
+            Queue<(int X, int Y)> points = new Queue<(int X, int Y)>();
+            points.Enqueue((location.X, location.Y));
+            Bitmap tempBmp = new Bitmap(_mainBmp);
+
+            while (points.Count != 0)
+            {
+                var currPoint = points.Dequeue();
+                if (currPoint.Y > pictureBox1.Height || currPoint.Y < 0
+                    || tempBmp.GetPixel(currPoint.X, currPoint.Y) != freeAreaColor)
+                {
+                    continue;
+                }
+                //right border finding            
+                var rightBorder = currPoint;
+                for (; rightBorder.X < pictureBox1.Width; rightBorder.X += 1)
+                {
+                    Color getPix = _mainBmp.GetPixel(rightBorder.X, rightBorder.Y);
+                    if (getPix != freeAreaColor)
+                    {
+                        break;
+                    }
+                    
+                    points.Enqueue((rightBorder.X, rightBorder.Y + 1));
+                    points.Enqueue((rightBorder.X, rightBorder.Y - 1));
+                    tempBmp.SetPixel(rightBorder.X, rightBorder.Y, _currentColor);
+                }
+                _mainBmp.SetPixel(rightBorder.X, rightBorder.Y, _currentColor);
+                // left border 
+                var leftBorder = currPoint;
+                leftBorder.X -= 1;
+
+                for (; leftBorder.X > 0; --leftBorder.X)
+                {
+                    Color getPix = _mainBmp.GetPixel(leftBorder.X, leftBorder.Y);
+
+                    if (getPix != freeAreaColor)
+                    {
+                        break;
+                    }
+                    tempBmp.SetPixel(leftBorder.X, leftBorder.Y, _currentColor);
+                    points.Enqueue((leftBorder.X, leftBorder.Y + 1));
+                    points.Enqueue((leftBorder.X, leftBorder.Y - 1));
+                }
+                _mainBmp.SetPixel(leftBorder.X, leftBorder.Y, _currentColor);
+            }
+        }
+
+        private void BFill(Point location)
+        {
+            Color freeAreaColor = _mainBmp.GetPixel(location.X, location.Y);
+            Queue<(int X, int Y)> points = new Queue<(int X, int Y)>();
+            points.Enqueue((location.X, location.Y));
+
+
+            while (points.Count != 0)
+            {
+                var currPoint = points.Dequeue();
+                if (currPoint.Y > pictureBox1.Height || currPoint.Y < 0
+                    || _mainBmp.GetPixel(currPoint.X, currPoint.Y) != freeAreaColor)
+                {
+                    continue;
+                }
+                //right border finding            
+                var rightBorder = currPoint;
+                for (; rightBorder.X < pictureBox1.Width; rightBorder.X += 1)
+                {
+                    Color getPix = _mainBmp.GetPixel(rightBorder.X, rightBorder.Y);
+                    if (getPix != freeAreaColor)
+                    {
+                        break;
+                    }
+                    Color fillColor =
+                        _fillPicture.GetPixel(rightBorder.X % _fillPicture.Width, rightBorder.Y % _fillPicture.Height);
+                    _mainBmp.SetPixel(rightBorder.X, rightBorder.Y, fillColor);
+                    points.Enqueue((rightBorder.X, rightBorder.Y + 1));
+                    points.Enqueue((rightBorder.X, rightBorder.Y - 1));
+                }
+
+                // left border 
+                var leftBorder = currPoint;
+                leftBorder.X -= 1;
+
+                for (; leftBorder.X > 0; --leftBorder.X)
+                {
+                    Color getPix = _mainBmp.GetPixel(leftBorder.X, leftBorder.Y);
+
+                    if (getPix != freeAreaColor)
+                    {
+                        break;
+                    }
+                    Color fillColor = _fillPicture.GetPixel(leftBorder.X % _fillPicture.Width, leftBorder.Y % _fillPicture.Height);
+                    _mainBmp.SetPixel(leftBorder.X, leftBorder.Y, fillColor);
+                    points.Enqueue((leftBorder.X, leftBorder.Y + 1));
+                    points.Enqueue((leftBorder.X, leftBorder.Y - 1));
+                }
+            }
+
+        }
+        private void AFill(Point location)
+        {
+            Color freeAreaColor = _mainBmp.GetPixel(location.X, location.Y);
+            Queue<(int X, int Y)> points = new Queue<(int X, int Y)>();
+            points.Enqueue((location.X, location.Y));
+
+
+            while (points.Count != 0)
+            {
+                var currPoint = points.Dequeue();
+                if (currPoint.Y > pictureBox1.Height || currPoint.Y < 0
+                    || _mainBmp.GetPixel(currPoint.X, currPoint.Y) != freeAreaColor)
+                {
+                    continue;
+                }
+                //right border finding            
+                var rightBorder = currPoint;
+                for (; rightBorder.X < pictureBox1.Width; rightBorder.X += 1)
+                {
+                    Color getPix = _mainBmp.GetPixel(rightBorder.X, rightBorder.Y);
+                    if (getPix != freeAreaColor)
+                    {
+                        break;
+                    }
+                    _mainBmp.SetPixel(rightBorder.X, rightBorder.Y, _currentColor);
+                    points.Enqueue((rightBorder.X, rightBorder.Y + 1));
+                    points.Enqueue((rightBorder.X, rightBorder.Y - 1));
+                }
+
+                // left border 
+                var leftBorder = currPoint;
+                leftBorder.X -= 1;
+
+                for (; leftBorder.X > 0; --leftBorder.X)
+                {
+                    Color getPix = _mainBmp.GetPixel(leftBorder.X, leftBorder.Y);
+
+                    if (getPix != freeAreaColor)
+                    {
+                        break;
+                    }
+                    _mainBmp.SetPixel(leftBorder.X, leftBorder.Y, _currentColor);
+                    points.Enqueue((leftBorder.X, leftBorder.Y + 1));
+                    points.Enqueue((leftBorder.X, leftBorder.Y - 1));
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_fillingState == FillingState.Draw)
+            {
+                _fillingState = FillingState.Default;
+            }
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_fillingState != FillingState.Draw)
+            {
+                lastPoint = e.Location;
+                return;
+            }
+            if (!lastPoint.IsEmpty)
+            {
+                _mainGraphics.DrawLine(_currentPen, lastPoint, e.Location);
+                _mainGraphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            }
+            lastPoint = e.Location;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            _fillingState = FillingState.SimpleFill;
+        }
+
+        private void FillPictureButton(object sender, EventArgs e)
+        {
+            _fillingState = FillingState.PictureFill;
+            if (_openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(_openFileDialog.FileName);
+
+                    SetPicture(_openFileDialog.FileName);
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void SetPicture(string fileName)
+        {
+            _fillPicture = new Bitmap(fileName);
+
+        }
+
+        private void DrawButton(object sender, EventArgs e)
+        {
+            _fillingState = FillingState.Default;
+        }
+
+        private void BorderFillButton(object sender, EventArgs e)
+        {
+            _fillingState = FillingState.BorderFill;
+        }
+    }
+}
